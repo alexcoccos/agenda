6211645d684a0603c82530e055f370a04421d7ff
Alex Contursi <contursi.axel@gmail.com>
2017-03-15 16:52:14 +0100

    funziona

--- agenda/agenda.service.js
+++ agenda/agenda.service.js
@@ -7,7 +7,8 @@ angular.module('app').service('AgendaSrv',function(){
     data: new Date(),
     descrizione: "hkdfbjkdavsfhdbs",
     luogo: "jhsdfanjd",
-    priorita: "#ffa500"
+    priorita: "#ffa500",
+    image: "http://scontent.cdninstagram.com/t51.2885-15/s480x480/e15/11427300_1031981593479479_60218334_n.jpg?ig_cache_key=MTAxNDAwNjE2MTUxMzIyNzg3NQ%3D%3D.2"
   },{
     titolo:"appuntamento lunedi",
     data: new Date(),
