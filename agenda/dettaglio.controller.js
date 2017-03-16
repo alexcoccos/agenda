3387965ca6f395da6fba07a191119bc3beb05fc9
Alex Contursi <contursi.axel@gmail.com>
2017-03-16 12:01:49 +0100

    aggiunta tasto indietro

--- agenda/dettaglio.controller.js
+++ agenda/dettaglio.controller.js
@@ -3,10 +3,12 @@ angular.module('app').controller('DettaglioCtrl',function($scope,AgendaSrv,$stat
 
   $scope.appuntamento= AgendaSrv.dettaglioAppuntamento($state.params.id);
 
-
-
   $scope.modifica= function(){
     $scope.appuntamento= AgendaSrv.aggiornaAppuntamento($scope.appuntamento);
+    $state.go('/');
+  }
+  $scope.back= function(){
+    $state.go('/');//questo significa vai alla pagnia iniziale tramite la rotta
   }
 
 });
